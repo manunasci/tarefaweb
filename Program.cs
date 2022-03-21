ef341ed0eac1cd2037be8d688aeebe12a4eb3802
manunasci <nascimentomanuella73@gmail.com>
2022-03-21 13:37:49 -0300

    esse commit adiciona o trabalho

--- Program.cs
+++ Program.cs
@@ -37,4 +37,54 @@ app.MapGet("/api/tarefas", ([FromServices] tarefasContext _db) =>
     return Results.Ok(_db.Tarefa.ToList<Tarefa>());
 });
 
+app.MapGet("/api/tarefas/{id}", (
+    [FromServices] tarefasContext _db,
+    [FromRoute] int id
+) =>
+{
+  //busca pelo id que está na rota
+  var tarefa =_db.Tarefa.Find(id);
+
+  //encontrou?
+  if (tarefa==null)
+    {
+      // não encontrou
+      //retornar um 404
+      return Results.NotFound();
+    }
+
+  // encontrou
+  //retornar 200 e os dados da tarefa
+  return Results.Ok(tarefa);
+});
+
+// Incluir nova tarefa
+app.MapPost("/api/tarefas",(
+ [FromServices] tarefasContext _db,
+ [FromBody] Tarefa novaTarefa
+) =>
+{
+  if (string.IsNullOrEmpty(novaTarefa.Descricao))
+  {
+      return Results.BadRequest();
+  }
+
+    if (novaTarefa.Concluida)
+  {
+      return Results.BadRequest();
+  }
+
+  var tarefa = new Tarefa{
+      Descricao = novaTarefa.Descricao,
+      Concluida = novaTarefa.Concluida,
+  };
+
+  _db.Tarefa.Add(tarefa);
+  _db.SaveChanges();
+
+  string urlTarefaCriada = $"/api/tarefas/{tarefa.Id}";
+
+  return Results.Created(urlTarefaCriada, tarefa);
+});
+
 app.Run();
